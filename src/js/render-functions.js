61c205f714e1bdfb8a1a0c144b2bd4f6a20578b9
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-06-09 19:11:30 +0300

    5

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -61,7 +61,7 @@ export function hideLoader() {
   console.log('-');
   setTimeout(() => {
     document.querySelector('.div-loader').classList.add('hidden');
-  },2000);
+  },1000);
 }
 
 export function toggleLoadMoreButton(show) {
