dff126d08065aa3296ab41ca24ee0ab7e210ecd6
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-06-09 19:24:14 +0300

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,7 +10,7 @@ export async function fetchImages(query, page = 1) {
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
-      safesearch: true,
+      safesearch: false,
       page: page,
       per_page: 15,
     },
